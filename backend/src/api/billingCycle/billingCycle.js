c57f2aad0af5fa5a4548f1e3920b6da344559d9b
Gabriel Gomes <gabrielrafael2508@hotmail.com>
2018-09-10 20:58:24 -0300

    add backend and start frontend

---
+++ backend/src/api/billingCycle/billingCycle.js
@@ -0,0 +1,32 @@
+const restful = require('node-restful');
+const mongoose = restful.mongoose;
+
+const creditSchema = new mongoose.Schema({
+	name: { type: String, required: true, },
+	value: { type: Number, min: 0, required: true, },
+});
+
+const debitSchema = new mongoose.Schema({
+	name: { type: String, required: true, },
+	value: {
+		type: Number,
+		min: 0,
+		required: [true, 'Informe o valor do débito'],
+	},
+	status: {
+		type: String,
+		required: false,
+		uppercase: true,
+		enum: ['PAGO', 'PENDENTE', 'AGENDADO'],
+	},
+});
+
+const billingCycleSchema = new mongoose.Schema({
+	name: { type: String, required: true, },
+	month: { type: Number, min: 1, max: 12, require: true, },
+	year: { type: Number, min: 1970, max: 2100, require: true, },
+	credits: [creditSchema],
+	debits: [debitSchema],
+});
+
+module.exports = restful.model('BillingCycle', billingCycleSchema);
