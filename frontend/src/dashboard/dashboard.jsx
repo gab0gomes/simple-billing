7afd62550c9601d33840f63aaf3eafcad6979185
Gabriel Gomes <gabrielrafael2508@hotmail.com>
2018-09-19 21:24:17 -0300

    add redux and add dashboard actions creators and reducers

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
+import { getSummary } from './dashboardActions'
 import ContentHeader from '../common/template/contentHeader';
 import Content from '../common/template/content';
 import ValueBox from '../common/widget/valueBox';
 import Row from '../common/layout/row'
 
 class Dashboard extends Component {
+	componentWillMount() {
+		this.props.getSummary();
+	};
+
 	render() {
+		const { credit, debit } = this.props.summary;
 		return (
 			<div>
 				<ContentHeader title='Dashboard' subtitle='Versão 1.0'/>
 				<Content>
 					<Row>
 						<ValueBox cols='12 4' color='green' icon='bank'
-							value='R$ 10' text='Total de Créditos'/>
+							value={ `R$ ${ credit }` } text='Total de Créditos'/>
 						<ValueBox cols='12 4' color='red' icon='credit-card'
-							value='R$ 10' text='Total de Débitos'/>
+							value={ `R$ ${ debit }` } text='Total de Débitos'/>
 						<ValueBox cols='12 4' color='blue' icon='money'
-							value='R$ 0' text='Valor Consolidado'/>
+							value={ `R$ ${ credit - debit }` } text='Valor Consolidado'/>
 					</Row>
 				</Content>
 			</div>
@@ -25,5 +33,12 @@ class Dashboard extends Component {
 	};
 };
 
-export default Dashboard;
+const mapStateToProps = state => ({
+	summary: state.dashboard.summary
+});
+
+const mapDispatchToProps = dispatch =>
+	bindActionCreators({ getSummary }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
 
---
+++ frontend/src/dashboard/dashboardReducer.js
@@ -0,0 +1,18 @@
+const INITIAL_STATE = {
+	summary: {
+		credit: 0,
+		debit: 0
+	}
+};
+
+export default function(state = INITIAL_STATE, action) {
+	switch (action.type) {
+		case 'BILLING_SUMMARY_FETCHED':
+			return {
+				...state,
+				summary: action.payload.data
+			}
+		default:
+			return state;
+	}
+};
