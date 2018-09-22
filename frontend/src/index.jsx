8742183206f8c03b14578225eb7876f29b3bfa86
Gabriel Gomes <gabrielrafael2508@hotmail.com>
2018-09-22 19:31:02 -0300

    add tabs navigation

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -7,7 +7,10 @@ import promise from 'redux-promise';
 import App from './main/app';
 import reducers from './main/reducers';
 
-const store = applyMiddleware(promise)(createStore)(reducers);
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+	&& window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = applyMiddleware(promise)(createStore)(reducers, devTools);
 
 ReactDOM.render(
 	<Provider store={ store }>
--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Router, Route, Redirect, hashHistory } from 'react-router';
 
-import Dashboard from '../dashboard/dashboard';
+// import Dashboard from '../dashboard/dashboard';
+import DashboardNoRedux from '../dashboardNoRedux/dashboardNoRedux';
 import BillingCycle from '../billingCycle/billingCycle';
 
 export default props => (
 	<Router history={ hashHistory }>
-		<Route path='/' component={ Dashboard }/>
+		{/* <Route path='/' component={ Dashboard }/> */}
+		<Route path='/' component={ DashboardNoRedux }/>
 		<Route path='/billingCycles' component={ BillingCycle }/>
 		<Redirect from='*' to='/'/>
 	</Router>
